a87470adc5595695a301217caa7b90da25506754
anir <anirdms@gmail.com>
2022-03-20 13:45:17 +0600

    user profile completed

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -185,3 +185,54 @@ export const pinDetailMorePinQuery = (pin) => {
         }`;
   return query;
 };
+
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+          image{
+            asset->{
+              url
+            }
+          },
+          _id,
+          destination,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+          save[]{
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+          },
+        }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+          image{
+            asset->{
+              url
+            }
+          },
+          _id,
+          destination,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+          save[]{
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+          },
+        }`;
+  return query;
+};
